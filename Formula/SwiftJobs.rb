588889d6c22558c9b59e21f9301227155578e738
Dave Paul <david_paul2@apple.com>
2022-09-06 12:47:30 -0500

    version bump

--- Formula/SwiftJobs.rb
+++ Formula/SwiftJobs.rb
@@ -1,10 +1,10 @@
 class Swiftjobs < Formula
   desc "A command line tool"
-  url "https://github.com/davepaul0/SwiftJobs/archive/refs/tags/0.1.1.zip"
-  version "0.1.1"
+  url "https://github.com/davepaul0/SwiftJobs/archive/refs/tags/0.2.0.zip"
+  version "0.2.0"
 
   def install
-    libexec.install Dir["bin/starter"]
+    libexec.install Dir["bin/MyJobs"]
     libexec.install "bin/jobs.sh" => "swiftjobs"
     bin.write_exec_script libexec/"swiftjobs"
   end
